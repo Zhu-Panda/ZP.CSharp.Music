36ac15731c94298445feefbd298d4c7b152fefa8
Zhu Panda <zhupandahugo@gmail.com>
2022-04-30 20:35:10 +0800

    add test project

---
+++ ZP.CSharp.Music.Tests/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ZP.CSharp.Music;
+using ZP.CSharp.Music.Tests;
+namespace ZP.CSharp.Music.Tests
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            new Piece(
+                new Voice(
+                    new Note(Pitch.Rest),
+                    new Note(Pitch.E4)
+                ),
+                new Voice(
+                    new Note(Pitch.C4, 3000)
+                )
+            ).Play();
+        }
+    }
+}
