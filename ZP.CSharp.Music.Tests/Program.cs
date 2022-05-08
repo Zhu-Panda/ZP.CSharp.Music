5ab1851b52f7756f2257298f10d39364704fe68b
Zhu Panda <zhupandahugo@gmail.com>
2022-05-08 21:38:49 +0800

    integrate components

--- ZP.CSharp.Music.Tests/Program.cs
+++ ZP.CSharp.Music.Tests/Program.cs
@@ -5,21 +5,24 @@ namespace ZP.CSharp.Music.Tests
 {
     public class Program
     {
-        public static void Main()
+        public static async Task Main()
         {
             Console.WriteLine(
                 "Choose song:\n" +
-                "1: La Marseillaise"
+                "1: La Marseillaise\n" + 
+                "2: Preußens Gloria"
             );
             var result = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Playing...");
-            Player.Play(
-                result switch
-                {
-                    1 => new LaMarseillaise().GetPiece(),
-                    _ => Piece.Empty
-                }
-            );
+            var piece = result switch
+            {
+                1 => new LaMarseillaise().GetPiece(),
+                2 => new PreußensGloria().GetPiece(),
+                _ => Piece.Empty
+            };
+            var play = Player.PlayAsync(piece);
+            var write = Writer.WriteAsync(piece, (str) => {Console.WriteLine(str);});
+            await Task.WhenAll(play, write);
             Console.WriteLine("Done.");
         }
     }
