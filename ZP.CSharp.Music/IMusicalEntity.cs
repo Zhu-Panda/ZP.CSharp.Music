74d02def974bc7eca8f6525c32b508abf244193f
Zhu Panda <zhupandahugo@gmail.com>
2022-05-08 11:35:58 +0800

    add lyric spec and unify entity structure

--- ZP.CSharp.Music/IMusicalEntity.cs
+++ ZP.CSharp.Music/IMusicalEntity.cs
@@ -3,5 +3,8 @@ using ZP.CSharp.Music;
 namespace ZP.CSharp.Music
 {
     public interface IMusicalEntity : IPlayable
-    {}
+    {
+        public List<IMusicalEntity> ChildEntities {get; set;}
+        public string Lyric {get; set;}
+    }
 }
