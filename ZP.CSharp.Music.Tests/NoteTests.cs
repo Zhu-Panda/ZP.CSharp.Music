ae4604b9278bae48ce95f579ea7cd7ec51fce1e9
Zhu Panda <zhupandahugo@gmail.com>
2022-05-09 16:07:11 +0800

    change tests to theories

--- ZP.CSharp.Music.Tests/NoteTests.cs
+++ ZP.CSharp.Music.Tests/NoteTests.cs
@@ -6,21 +6,23 @@ namespace ZP.CSharp.Music.Tests.NoteTests
 {
     public class Assignments
     {
-        [Fact]
-        public void BPMCanOnlyBeSetWithSetBPM()
+        [Theory]
+        [InlineData(123)]
+        [InlineData(456)]
+        [InlineData(7890)]
+        public void BPMCanOnlyBeSetWithSetBPM(double bpm)
         {
-            const int bpm = 120;
             var note = new Note(Pitch.Rest, Duration.Crotchet);
             Assert.NotEqual(bpm, note.BPM);
             note.SetBPM(bpm);
             Assert.Equal(bpm, note.BPM);
         }
-        [Fact]
-        public void NormalPropertiesCanBeSet()
+        [Theory]
+        [InlineData(Pitch.Rest, Duration.Crotchet, "1, 2, 3!")]
+        [InlineData(Pitch.C4, Duration.Quaver, "4, 5, 6!")]
+        [InlineData(Pitch.G5, Duration.Breve, "7, 8, 9, 0!")]
+        public void NormalPropertiesCanBeSet(Pitch pitch, Duration duration, string lyric)
         {
-            const Pitch pitch = Pitch.Rest;
-            const Duration duration = Duration.Crotchet;
-            const string lyric = "Panda!";
             var note = new Note(pitch, duration, lyric);
             Assert.Equal(pitch, note.Pitch);
             Assert.Equal(duration, note.Duration);
