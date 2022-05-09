22318681f1868f2fb7787c53ffbbd0cadc7433e5
Zhu Panda <zhupandahugo@gmail.com>
2022-05-09 16:07:18 +0800

    add more tests

---
+++ ZP.CSharp.Music.Tests/WriterTests.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+using ZP.CSharp.Music;
+using ZP.CSharp.Music.Tests.WriterTests;
+namespace ZP.CSharp.Music.Tests.WriterTests
+{
+    public class LyricOutput
+    {
+        [Theory]
+        [InlineData("1, 2, 3!")]
+        [InlineData("4, 5, 6!")]
+        [InlineData("7, 8, 9, 0!")]
+        public void WriterOutputsNoteLyric(string lyric)
+        {
+            var note = new Note(Pitch.Rest, Duration.Crotchet, lyric);
+            var result = string.Empty;
+            Writer.Write(note, (str) => result = str);
+            Assert.Equal(lyric, result);
+        }
+    }
+}
